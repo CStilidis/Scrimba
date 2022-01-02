bbb2b70931de7188c1bc0715b5f9dfa771b7748b
CStilidis <cstilidis@gmail.com>
2022-01-02 20:00:32 +0200

    Making finction work

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,17 +1,15 @@
 let myLeads = ["www.google.com", "www.dropbox", "www.twitter.com"];
+
 const ulEl = document.getElementById("ul-el");
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
 });
 
-let listItems = "";
-for (i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>";
-  
-}
-console.log(myLeads);
-ulEl.innerHTML = myLeads;
