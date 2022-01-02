edd9693380cd496875eb1444f622a4d2fc6e1755
CStilidis <cstilidis@gmail.com>
2022-01-03 00:59:36 +0200

    Save Changes

--- Chrome Extension/app.js
+++ Chrome Extension/app.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.google.com", "www.dropbox", "www.twitter.com"];
+let myLeads = [];
 
 const ulEl = document.getElementById("ul-el");
 const inputEl = document.getElementById("input-el");
@@ -6,10 +6,15 @@ const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  renderLeads();
+});
+
+
+function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += "<li>" + myLeads[i] + "</li>";
   }
   ulEl.innerHTML = listItems;
-});
-
+}
+console.log(listItems);
--- Clipboard/index.js
+++
@@ -1,7 +0,0 @@
-let containerEl = document.getElementById("container")
-
-containerEl.innerHTML = "<button>Buy!</button>"
-
-containerEl.addEventListener("click", function(){
-    containerEl.innerHTML += "</br>" + "<p style= color:red;>Thank you for buying!</p>"
-})
