040681d9be6621b9e3ad84636c3a8aa6a8e4ec07
CStilidis <cstilidis@gmail.com>
2022-02-07 22:55:51 +0200

    save changes

--- Practice/jsObject.js
+++ Practice/jsObject.js
@@ -1,19 +1,29 @@
-let animal = {}
-animal.name = 'Leo'
-animal.energy = 10
-
-animal.eat = function (amount) {
-    console.log(`${this.name} is eating.`)
-    this.energy += amount
+const animalMethods = {
+    eat(amount) {
+        console.log(`${this.name} is eating.`)
+        this.energy += amount
+    },
+    sleep(length) {
+        console.log(`${this.name} is sleeping`)
+        this.energy +=length
+    },
+    play(length) {
+        console.log(`${this.name} is playing`)
+        this.energy -= length
+    }
 }
 
-animal.sleep = function (length) {
-    console.log(`${this.name} is sleeping.`)
-    this.energy += length
+function Animal (name, energy) {
+    let animal = {}
+    animal.name = name
+    animal.energy = energy
+    animal.eat = animalMethods.eat
+    animal.sleep = animalMethods.sleep
+    animal.play = animalMethods.play
+    
+    return animal
+
 }
 
-animal.play = function (length) {
-    console.log(`${this.name} is playing.`)
-    this.energy -=length
-}
-
+const leo = Animal("Leo", 7)
+const snoop = Animal("Snoop", 10)
