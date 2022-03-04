e976fb1ad521f0a7199df03637df21ab2fa54944
Christos <cstilidis@gmail.com>
2022-03-04 02:56:06 +0200

    save changes

--- Clipboard/app.js
+++ Clipboard/app.js
@@ -1,28 +1,22 @@
-function Animal (name, energy) {
-    let animal = Object.create(Animal.prototype);
-    animal.name = name;
-    animal.energy = energy
+class Animal {
+    constructor(name, energy) {
+      this.name = name
+      this.energy = energy
+    }
+    eat(amount) {
+      console.log(`${this.name} is eating.`)
+      this.energy += amount
+    }
+    sleep(length) {
+      console.log(`${this.name} is sleeping.`)
+      this.energy += length
+    }
+    play(length) {
+      console.log(`${this.name} is playing.`)
+      this.energy -= length
+    }
+  }
+  
+  const leo = new Animal('Leo', 7)
+  const snoop = new Animal('Snoop', 10)
 
-    return animal;
-}
-
-Animal.prototype.eat = function (amount) {
-    console.log(`${this.name} is eating.`);
-    this.energy += amount;
-}
-
-Animal.prototype.sleep = function (length) {
-    console.log(`${this.name} is sleeping`)
-    this.energy += length
-}
-
-Animal.prototype.play = function (length) {
-    console.log(`${this.name} is playing`)
-    this.energy -= length;
-}
-
-const leo = new Animal('Leo', 7)
-const snoop = new Animal('Snoop', 10)
-
-leo.eat(10)
-snoop.play(5)
