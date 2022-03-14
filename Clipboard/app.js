5ba6a8376ee0335de82f003a771371dbcf73ef95
Christos <cstilidis@gmail.com>
2022-03-15 00:31:14 +0200

    save changes

--- Clipboard/app.js
+++ Clipboard/app.js
@@ -1,28 +1,12 @@
-class Animal {
-  constructor(name, energy){
-    this.name = name;
-    this.energy = energy
-  }
-
-  eat(amount) {
-    console.log(`${this.name} is eating`)
-    this.energy += amount;
-  }
-
-  sleep(length) {
-    console.log(`${this.name} is sleeping`)
-    this.energy += length
-  }
-
-  play(length) {
-    console.log(`${this.name} is playing`)
-    this.energy -= length
-  }
-
+function Animal (name, energy) {
+  this.name = name;
+  this.energy = energy;
 }
 
-const myObj = new Animal('Chris', 10)
+function user() {}
 
-myObj.prototype = new Animal()
-const result = Object.getPrototypeOf(myObj)
-console.log(result)
+const chris = new Animal('Chris', 10);
+
+
+
+
