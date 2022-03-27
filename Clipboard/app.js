40d44a15540e84ac2a2714a4d0a8114da2f054bb
Christos <cstilidis@gmail.com>
2022-03-27 22:52:11 +0300

    Prototypes

--- Clipboard/app.js
+++ Clipboard/app.js
@@ -1,5 +1,19 @@
-const Animal = () => {}
+function Animal(name, energy) {
+    let animal = {}
+    this.name = name;
+    this.energy = energy;
 
-const leo = new Animal()
+    animal.play = animalMethods.play
 
- 
+    return animal;
+}
+
+let animalMethods = {
+   play (amount) {
+       energy -= amount;
+       console.log(`${this.name} is playing`)
+   }
+}
+
+let chris = Animal('chris', 36)
+chris.play(5) 
